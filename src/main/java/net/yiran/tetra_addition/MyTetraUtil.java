82bbf3ed15039a0bcac09504aa7ad65190b58c76
yiran1457 <3270897489@qq.com>
2025-03-29 09:24:13 +0800

    修改添加了一部分方法

--- src/main/java/net/yiran/tetra_addition/MyTetraUtil.java
+++ src/main/java/net/yiran/tetra_addition/MyTetraUtil.java
@@ -1,19 +1,20 @@
 package net.yiran.tetra_addition;
 
+import dev.latvian.mods.kubejs.typings.Info;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.yiran.tetra_addition.util.IConfigSchematic;
 import net.yiran.tetra_addition.util.ISchematicRegistry;
 import org.jetbrains.annotations.NotNull;
 import se.mickelus.tetra.module.SchematicRegistry;
 import se.mickelus.tetra.module.schematic.ConfigSchematic;
+import se.mickelus.tetra.module.schematic.OutcomeDefinition;
+import se.mickelus.tetra.module.schematic.SchematicDefinition;
 import se.mickelus.tetra.module.schematic.UpgradeSchematic;
 
 import javax.annotation.Nullable;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class MyTetraUtil {
     public static MyTetraUtil instance = new MyTetraUtil();
@@ -43,6 +44,19 @@ public class MyTetraUtil {
         return ((IConfigSchematic) schematic).getDefinition().outcomes[0].material.count;
     }
 
+    @Info("精准确定outcomeItemStack的材料数量")
+    public ItemStack getModuleMaterial(@NotNull ItemStack itemStack, String slot,ItemStack outcomeItemStack) {
+        ConfigSchematic schematic = getModuleKey2Schematic().get(getModuleKey(itemStack, slot));
+        if (schematic == null) return ItemStack.EMPTY;
+        for (OutcomeDefinition outcome : ((IConfigSchematic) schematic).getDefinition().outcomes) {
+            for (ItemStack stack : outcome.material.getApplicableItemStacks()) {
+                if(stack.getItem()==outcomeItemStack.getItem())
+                    return stack;
+            }
+        }
+        return ItemStack.EMPTY;
+    }
+
     public Map<String, ConfigSchematic> getModuleKey2Schematic() {
         if (moduleKey2Schematic == null) {
             return getAndUpdateModuleKey2Schematic();
@@ -50,13 +64,20 @@ public class MyTetraUtil {
         return moduleKey2Schematic;
     }
 
+    @Info("当你使用了reload指令会导致Schematic重新构造,于是便需要使用这个方法")
     public Map<String, ConfigSchematic> getAndUpdateModuleKey2Schematic() {
         HashMap<String, ConfigSchematic> moduleKey2Schematic = new HashMap<>();
         Set<ConfigSchematic> schematicsSet = new HashSet<>();
         getSchematicMap().forEach((k, v) -> {
             if (v instanceof ConfigSchematic configSchematic && !v.isHoning()) {
-                String moduleKey = ((IConfigSchematic) configSchematic).getDefinition().outcomes[0].moduleKey;
+                SchematicDefinition schematicDefinition = ((IConfigSchematic) configSchematic).getDefinition();
+                String moduleKey = schematicDefinition.outcomes[0].moduleKey;
                 if (moduleKey != null) {
+                    if (schematicDefinition.keySuffixes.length > 0) {
+                        for (String keySuffix : schematicDefinition.keySuffixes) {
+                            moduleKey2Schematic.put(moduleKey + keySuffix, configSchematic);
+                        }
+                    }
                     moduleKey2Schematic.put(moduleKey, configSchematic);
                     schematicsSet.add(configSchematic);
                 }
